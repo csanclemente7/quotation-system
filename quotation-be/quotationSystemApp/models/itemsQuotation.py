8594814a59765126bd4ad9c38e78689af348fa76
Michael Hernández <mghernandezcastillo@gmail.com>
2022-05-16 17:39:03 -0500

    update

--- quotation-be/quotationSystemApp/models/itemsQuotation.py
+++ quotation-be/quotationSystemApp/models/itemsQuotation.py
@@ -6,8 +6,10 @@ class ItemsQuotation(models.Model):
     id = models.BigAutoField(primary_key=True)
     quotation = models.ForeignKey(Quotation, on_delete=models.CASCADE)
     item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    price = models.FloatField('price', default=0)
+    name = models.CharField(max_length=50, default='')
     quantity = models.IntegerField('quantity', default=0)
     total = models.FloatField('total', default=0)
 
     def __str__(self):
-        return str(self.id) + ' ' + str(self.quotation) + ' ' + str(self.item) + ' ' + str(self.quantity) + ' ' + str(self.total)
+        return str(self.id) + ' ' + str(self.quotation) + ' ' + str(self.item) + ' ' + str(self.price) + ' ' + str(self.name) + ' ' + str(self.quantity) + ' ' + str(self.total)
