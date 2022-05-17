aa691b7cab4ed7dcdea0c89872920e4f3865f388
Christian Sanclemente <cfsanclemente@gmail.com>
2022-05-17 14:45:30 -0500

    button color blue

--- quotation-fe/src/components/Home.vue
+++ quotation-fe/src/components/Home.vue
@@ -5,9 +5,15 @@
         <img src="../assets/img/home_img.png" alt="" />&nbsp; App Cotizaciones
       </h1>
       <div class="input-container">
-        <button v-on:click="openPopUp('quotation')" class="button">
+        <button v-on:click="openPopUp('quotation')" class="button blue">
           Nueva Cotización
         </button>
+
+        <div class="input-container">
+          <button v-on:click="openPopUp('quotation')" class="button">
+            Insumos
+          </button>
+        </div>
       </div>
     </div>
   </div>
@@ -275,7 +281,7 @@ export default {
     quotationServices.getQuotationsList().then((result) => {
       this.quotations = result;
     });
-    for (let i = 0; i < 5; i++) {
+    for (let i = 0; i < 4; i++) {
       this.createItemQuotation();
     }
   },
