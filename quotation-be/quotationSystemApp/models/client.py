991e99c60770aa390cba407ce0f48cf99dd8fbeb
Christian Sanclemente <cfsanclemente@gmail.com>
2022-06-14 15:21:58 -0500

    update

--- quotation-be/quotationSystemApp/models/client.py
+++ quotation-be/quotationSystemApp/models/client.py
@@ -6,8 +6,8 @@ class Client(models.Model):
     name = models.CharField('name',max_length=100)
     city = models.CharField('city',max_length=100)
     address = models.CharField('address',max_length=100)
-    email = models.EmailField('email',max_length=100)
-    phone = models.CharField('phone',max_length=100, default=None)
+    email = models.CharField('email',max_length=100,blank=True, null=True)
+    phone = models.CharField('phone',max_length=100, default=None,blank=True, null=True)
 
     def __str__(self):
         return str(self.id) + ' ' + str(self.name) + ' ' + str(self.city) + ' ' + str(self.address) + ' ' + str(self.email) + ' ' + str(self.phone)
